a571ea0e9bf626418839834302d92c8205921471
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-08 22:00:18 +0530

    Example of JPQL

--- E-com-project/src/main/java/com/example/sanjaykumar_777/E_com/project/repository/ProductRepository.java
+++ E-com-project/src/main/java/com/example/sanjaykumar_777/E_com/project/repository/ProductRepository.java
@@ -2,7 +2,17 @@ package com.example.sanjaykumar_777.E_com.project.repository;
 
 import com.example.sanjaykumar_777.E_com.project.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product,Integer> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR" +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR" +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR" +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+    )
+    List<Product> searchProducts(String keyword);
 }
